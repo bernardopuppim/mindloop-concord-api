597b0effffb83cad9e94190a074185284c60a652
Bernardo Puppim <bernardo.puppim@gmail.com>
2025-12-15 00:57:38 -0300

    test: isolate serverless handler 2

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,16 @@
-
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { createApp } from "../server/app";
+
+let app: any;
 
 export default async function handler(
-  _req: VercelRequest,
+  req: VercelRequest,
   res: VercelResponse
 ) {
-  return res.status(200).json({ ok: true, isolated: true });
+  if (!app) {
+    app = await createApp();
+  }
+
+  return app(req, res);
 }
+
